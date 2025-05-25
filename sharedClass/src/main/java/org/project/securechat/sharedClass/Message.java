e33807d08555ff86706d4685df09f9715218301a
Kacpron123 <kacper030904@gmail.com>
2025-05-25 12:35:50 +0200

    Message class

--- sharedClass/src/main/java/org/project/securechat/sharedClass/Message.java
+++ sharedClass/src/main/java/org/project/securechat/sharedClass/Message.java
@@ -1,16 +1,23 @@
 package org.project.securechat.sharedClass;
-
+import java.time.Instant;
 /**
  * Hello world!
  *
  */
 public class Message
 {
-    public Message(String data){
-      this.data = data;
-    }
-    public String data;
-    public void write(){
-      System.out.println(data);
-    }
+  public Message(String senderID, String chatID, String data){
+    this.senderID = senderID;
+    this.chatID = chatID;
+    this.timestamp = Instant.now();
+    this.data = data;
+  }
+  public String senderID;
+  public String chatID;
+  public Instant timestamp;
+  // public byte[] message;
+  public String data; //for now 
+  public void write(){
+    System.out.println(data);
+  }
 }
