842756891e8c668f99f121b61baf1cf82531df91
Psaidu <ekurdziel@student.agh.edu.pl>
2025-06-11 19:01:24 +0200

    Added user exists / active and encryption messages

--- client/src/main/java/org/project/securechat/client/Client.java
+++ client/src/main/java/org/project/securechat/client/Client.java
@@ -58,7 +58,7 @@ public class Client {
           break;
         }
         clientOutputQueue.put(messageForServer);
-        Thread.sleep(500);
+        Thread.sleep(2000);
         String response = Client.status.poll();
         LOGGER.info("Status {}", response);
         if (response != null && response.equals("OK")) {
